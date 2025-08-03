e15374fc8bb09b8a28958168b8def17be9040040
anthonyckleung <anthonyleung124@gmail.com>
2025-08-03 18:23:57 -0400

    created classifier mcp server

---
+++ src/classifier_mcp_server/main.py
@@ -0,0 +1,46 @@
+from fastmcp import FastMCP
+from minio import Minio
+import pickle
+from src.classifier_mcp_server.utils import job_description_classifier
+
+mcp = FastMCP("items")
+
+minio_client = Minio(
+    endpoint = "minio:9000",
+    access_key= "minioadmin",
+    secret_key= "minioadmin",
+    secure=False
+)
+
+response = minio_client.get_object('fraud-classifier', 'count_vectorizer.pkl')
+obj_bytes = response.read()
+
+COUNT_VECTORIZER = pickle.loads(obj_bytes)
+
+response = minio_client.get_object('fraud-classifier', 'model.pkl')
+obj_bytes = response.read()
+
+MODEL = pickle.loads(obj_bytes)
+
+
+@mcp.tool()
+def get_prediction(text:str) -> dict:
+    """Classifies if a given job posting is real or fake.
+    Uses the top retrieved context from get_formatted_context as input
+    and return classification_result.
+
+    Args:
+        text: Input string of the job posting from retrieved_context from get_formatted_context
+    
+    Returns:
+        A dictionary of the classification label and prediction probability score.
+    """
+    if not text:
+        return {"classification_result": {"error": "Missing job description."}}
+
+    prediction_result = job_description_classifier(MODEL, COUNT_VECTORIZER, text)
+    return prediction_result
+
+
+if __name__ == "__main__":
+    mcp.run(transport='http', host="0.0.0.0", port=8000)
